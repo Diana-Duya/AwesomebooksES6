fef42a31aa838751bc64ab0c17a1758beb6c7c90
unknown <dianaduya2@gmail.com>
2022-10-05 13:11:50 +0100

    Use ES6 syntax

---
+++ modules/Datime.js
@@ -0,0 +1,7 @@
+export default  class Datime {
+    dateTime = () => {
+      dateTime.innerHTML = `
+          <h4>${DateTime.now().toLocaleString(DateTime.DATETIME_HUGE_WITH_SECONDS)}</h4>
+      `;
+    };
+  }
